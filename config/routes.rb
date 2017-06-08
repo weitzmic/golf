bbb3876d19fb249d68d8555fa9473682bb805b73
Michael Weitz <mweitz0@chicagobooth.edu>
2017-06-08 17:37:35 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -100,6 +100,39 @@ Rails.application.routes.draw do
   get("/street_to_weather/new", { :controller => "meteorologist", :action => "street_to_weather_form" })
   get("/street_to_weather", { :controller => "meteorologist", :action => "street_to_weather" })
 
-  
+
+  # Routes for the Comment resource:
+# CREATE
+get "/comments/new", :controller => "comments", :action => "new"
+post "/quick_create_comment", :controller => "comments", :action => "create"
+
+# READ
+get "/comments", :controller => "comments", :action => "index"
+get "/comments/:id", :controller => "comments", :action => "show"
+
+# UPDATE
+get "/comments/:id/edit", :controller => "comments", :action => "edit"
+post "/update_comment/:id", :controller => "comments", :action => "update"
+
+# DELETE
+get "/delete_comment/:id", :controller => "comments", :action => "destroy"
+#------------------------------
+
+# Routes for the Like resource:
+# CREATE
+get "/my_likes/new", :controller => "likes", :action => "new"
+post "/quick_create_my_like", :controller => "likes", :action => "create"
+
+# READ
+get "/my_likes", :controller => "likes", :action => "index"
+get "/my_like/:id", :controller => "likes", :action => "show"
+
+# UPDATE
+get "/my_likes/:id/edit", :controller => "likes", :action => "edit"
+post "/update_my_like/:id", :controller => "likes", :action => "update"
+
+# DELETE
+get "/quick_delete_my_like/:id", :controller => "likes", :action => "destroy"
+#------------------------------
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
