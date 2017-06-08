2313c14d8121e9809378466a40cbc93f47712c30
Michael Weitz <mweitz0@chicagobooth.edu>
2017-06-07 19:53:52 -0500

    update

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,5 +7,6 @@ has_many :scores, :through => :holes, :source => :scores
 has_many :weathers, :through => :scores, :source => :weather
 
 validates :course_name, :presence => true
+validates :course_year_started, presence: true, format: {with: /(19|20)\d{2}/i }
 
 end
