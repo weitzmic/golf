929f84cd0fd18916a4c37e546c750abe313d3a2d
Michael Weitz <mweitz0@chicagobooth.edu>
2017-06-10 10:21:23 -0500

    updates

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,7 +26,7 @@ class CommentsController < ApplicationController
     save_status = @comment.save
 
     if save_status == true
-      redirect_to("/comments/#{@comment.id}", :notice => "Comment created successfully.")
+      redirect_to("/courses", :notice => "Note created successfully.")
     else
       render("comments/new.html.erb")
     end
